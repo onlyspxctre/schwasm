56fc17b50fdc9159fa8d1e18ff4255c37df88782
onlyspxctre <onlyspxctre@proton.me>
2026-08-14 16:54:06 -0400

    schwasm: preprocessor directives `DC.B` and `DS.B`
    A rudimentary implementation of these directives. They are not perfect
    nor 1:1 with the official implementation within GCPU-IDE.

--- schwasm.c
+++ schwasm.c
@@ -6,8 +6,10 @@
 #define ROM_SIZE 4096
 
 #define SCHWASM_FILE_FMT "%s:%ld:%ld:"
-    #define schwasm_file_arg(name, tok_line) name, tok_line.line, tok_line.col
+#define schwasm_file_arg(name, tok_line) name, tok_line.line, tok_line.col
 
+// TODO: instead of generating dumb nodes, we should generate abstract syntax.
+// This will allow us to decouple the interpreter side from the IR/machine-code generator.
 struct Schwasm_Node {
     uint16_t addr;
     uint8_t code;
@@ -47,8 +49,7 @@ static inline void schwasm_expect_org(struct Schwasm *schwasm) {
     if (schwasm->addr == UINT16_MAX) {
         Sp_Lexer_Token_Line token_line = splexer_token_get_line(&schwasm->lexer, schwasm_get_token(schwasm));
         sp_die(1, SCHWASM_FILE_FMT " No preceding ORG to define initial address\n", schwasm_file_arg(schwasm->filename, token_line));
-    }
-    else if (schwasm->addr >= ROM_SIZE) {
+    } else if (schwasm->addr >= ROM_SIZE) {
         Sp_Lexer_Token_Line token_line = splexer_token_get_line(&schwasm->lexer, schwasm_get_token(schwasm));
         sp_die(1, SCHWASM_FILE_FMT " Address out of bounds\n", schwasm_file_arg(schwasm->filename, token_line));
     }
@@ -70,6 +71,14 @@ static inline const Sp_Lexer_Token *schwasm_get_token(struct Schwasm *schwasm) {
         return NULL;
     }
     return &schwasm->lexer.tokens.data[schwasm->lexer_attr.idx];
+}
+
+static inline const Sp_Lexer_Token *schwasm_peek_token(struct Schwasm *schwasm) {
+    if (schwasm->lexer_attr.idx + 1 >= schwasm->lexer.tokens.count) {
+        return NULL;
+    }
+
+    return &schwasm->lexer.tokens.data[schwasm->lexer_attr.idx + 1];
 }
 
 static inline const Sp_Lexer_Token *schwasm_next_token(struct Schwasm *schwasm) {
@@ -218,6 +227,96 @@ void ld(struct Schwasm *schwasm, enum GCPU_REG reg) {
     }
 }
 
+enum Declare_Directive {
+    DC_B,
+    DS_B
+};
+
+// TODO: Preprocessor directives with a period in their identifier is currently parsed as separate tokens.
+// This is because Splexer currently does not support periods in identifiers.
+// As a result, this means that something like "DC   .   B" with whitespace in the middle of the directive is completely valid.
+// We must fix this on the Splexer side; we will probably implement a compile flag that enables periods within identifier names.
+void declare_directive(struct Schwasm *schwasm, enum Declare_Directive directive) {
+    const Sp_Lexer_Token *prev = schwasm_get_token(schwasm);
+    const Sp_Lexer_Token *token = schwasm_next_token(schwasm);
+
+    Sp_Lexer_Token_Line prev_line = splexer_token_get_line(&schwasm->lexer, prev);
+    Sp_Lexer_Token_Line token_line = splexer_token_get_line(&schwasm->lexer, token);
+
+    if (!token || prev_line.line != token_line.line || token->type != TOK_Period) {
+        sp_die(1, SCHWASM_FILE_FMT " Failed to parse assembly directive \"" SP_SV_FMT "\"", schwasm_file_arg(schwasm->filename, prev_line), sp_sv_arg(prev->sv));
+    }
+
+    prev = token;
+    token = schwasm_next_token(schwasm);
+    prev_line = token_line;
+    token_line = splexer_token_get_line(&schwasm->lexer, token);
+
+    if (!token || prev_line.line != token_line.line || !sp_sv_eq(&sp_cstr_slice("B"), &token->sv)) {
+        sp_die(1, SCHWASM_FILE_FMT " Failed to parse assembly directive", schwasm_file_arg(schwasm->filename, prev_line));
+    }
+
+    switch (directive) {
+        case DC_B:
+        dc_loop:
+            switch (schwasm_expect_value(schwasm)) {
+                case SCHWASM_VALUE_HEX:
+                    if (schwasm_get_token(schwasm)->sv.count > 2) {
+                        Sp_Lexer_Token_Line token_line = splexer_token_get_line(&schwasm->lexer, schwasm_get_token(schwasm));
+                        sp_die(1, SCHWASM_FILE_FMT " ROM address does not support %d-bit integers\n", schwasm_file_arg(schwasm->filename, token_line), (int) powl(2, schwasm_get_token(schwasm)->sv.count));
+                    }
+
+                    errno = 0;
+                    int value = (int) strtol(schwasm_get_token(schwasm)->sv.ptr, NULL, 16);
+
+                    if (errno != 0) {
+                        Sp_Lexer_Token_Line token_line = splexer_token_get_line(&schwasm->lexer, schwasm_get_token(schwasm));
+                        sp_die(1, SCHWASM_FILE_FMT " Error parsing hexadecimal value\n", schwasm_file_arg(schwasm->filename, token_line));
+                    }
+
+                    schwasm_create_node(schwasm, (uint8_t) value);
+                    break;
+                case SCHWASM_VALUE_DECIMAL:
+                    schwasm_create_node(schwasm, (uint8_t) schwasm_get_token(schwasm)->int_lit.value);
+                    break;
+            }
+
+            const Sp_Lexer_Token *next = schwasm_peek_token(schwasm);
+            Sp_Lexer_Token_Line next_line = splexer_token_get_line(&schwasm->lexer, next);
+            if (next->type == TOK_Comma && splexer_token_get_line(&schwasm->lexer, schwasm_get_token(schwasm)).line == next_line.line) {
+                schwasm_next_token(schwasm);
+                goto dc_loop;
+            }
+            break;
+        case DS_B:
+            switch (schwasm_expect_value(schwasm)) {
+                case SCHWASM_VALUE_HEX:
+                    errno = 0;
+                    unsigned int value = (unsigned int) strtol(schwasm_get_token(schwasm)->sv.ptr, NULL, 16);
+
+                    if (errno != 0) {
+                        Sp_Lexer_Token_Line token_line = splexer_token_get_line(&schwasm->lexer, schwasm_get_token(schwasm));
+                        sp_die(1, SCHWASM_FILE_FMT " Error parsing hexadecimal value\n", schwasm_file_arg(schwasm->filename, token_line));
+                    }
+
+                    // TODO: turn this into a span definition instead of spamming memory segment definitions
+                    for (unsigned int i = 0; i < value; ++i) {
+                        schwasm_create_node(schwasm, 0x00);
+                    }
+                    break;
+                case SCHWASM_VALUE_DECIMAL:
+                    value = (unsigned int) schwasm_get_token(schwasm)->int_lit.value;
+
+                    // TODO: turn this into a span definition instead of spamming memory segment definitions
+                    for (unsigned int i = 0; i < value; ++i) {
+                        schwasm_create_node(schwasm, 0x00);
+                    }
+                    break;
+            }
+            break;
+    }
+}
+
 static Sp_String_Builder generated = {0};
 static struct Schwasm schwasm = {0};
 
@@ -319,6 +418,10 @@ int main(int argc, char **argv) {
             schwasm_create_node(&schwasm, 0x30);
         } else if (sp_sv_eq(&sp_cstr_slice("INY"), &token->sv)) {
             schwasm_create_node(&schwasm, 0x31);
+        } else if (sp_sv_eq(&sp_cstr_slice("DC"), &token->sv)) {
+            declare_directive(&schwasm, DC_B);
+        } else if (sp_sv_eq(&sp_cstr_slice("DS"), &token->sv)) {
+            declare_directive(&schwasm, DS_B);
         } else {
             sp_die(1, SCHWASM_FILE_FMT " Failed to parse unknown instruction \"" SP_SV_FMT "\"\n", schwasm_file_arg(schwasm.filename, token_line), sp_sv_arg(token->sv));
         }
